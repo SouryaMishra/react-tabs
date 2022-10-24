ed8fb2a01f00344dae2a429ea759140e81933c37
Sourya Mishra <sourya.mishra111@gmail.com>
2022-10-25 01:04:39 +0530

    add keyboard navigation

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef, useEffect } from "react";
 import { IBaseProps } from "../../baseProps";
 import { ITabGroupContext, useTabGroupContext } from "../../contexts";
 import cn from "classnames";
@@ -7,26 +8,38 @@ export interface ITabProps extends IBaseProps {
   value: string | number;
 }
 
-export const Tab = ({ value, className, children }: ITabProps) => {
-  const { value: currentValue, onChange }: ITabGroupContext =
-    useTabGroupContext()!;
-  const isActive = value === currentValue;
-  const classNames = cn(
-    "tab",
-    {
-      "tab--active": isActive,
-    },
-    className
-  );
-  return (
-    <button
-      role="tab"
-      className={classNames}
-      onClick={(e) => onChange(value, e)}
-      aria-selected={isActive}
-      tabIndex={isActive ? 0 : -1}
-    >
-      {children}
-    </button>
-  );
-};
+interface IClonedTabProps {
+  onKeyDown?: (e: React.KeyboardEvent<HTMLButtonElement>) => void;
+}
+
+export const Tab = forwardRef<HTMLButtonElement, ITabProps>(
+  (
+    { value, className, children, onKeyDown }: ITabProps & IClonedTabProps,
+    ref
+  ) => {
+    const { value: currentValue, onChange }: ITabGroupContext =
+      useTabGroupContext()!;
+    const isActive = value === currentValue;
+    const classNames = cn(
+      "tab",
+      {
+        "tab--active": isActive,
+      },
+      className
+    );
+
+    return (
+      <button
+        ref={ref}
+        role="tab"
+        className={classNames}
+        onClick={(e) => onChange(value, e)}
+        aria-selected={isActive}
+        tabIndex={isActive ? 0 : -1}
+        onKeyDown={onKeyDown}
+      >
+        {children}
+      </button>
+    );
+  }
+);
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from "react";
 import { IBaseProps } from "../../baseProps";
 import cn from "classnames";
 import "./Tabs.scss";
@@ -5,9 +6,43 @@ import "./Tabs.scss";
 export interface ITabsProps extends IBaseProps {}
 
 export const Tabs = ({ className, children }: ITabsProps) => {
+  const tabsRef = useRef<HTMLElement[]>([]);
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    const tabsLength = tabsRef.current.length;
+    if (e.key === "ArrowRight") {
+      const index = tabsRef.current.findIndex((tab) => tab === e.target);
+
+      index > -1 && index < tabsLength - 1
+        ? tabsRef.current[index + 1].focus()
+        : tabsRef.current[0].focus();
+    }
+    if (e.key === "ArrowLeft") {
+      const index = tabsRef.current.findIndex((tab) => tab === e.target);
+
+      index > -1 && index > 0
+        ? tabsRef.current[index - 1].focus()
+        : tabsRef.current[tabsLength - 1].focus();
+    }
+    if (e.key === "Home") {
+      e.preventDefault(); // To prevent page from scrolling up
+      const index = tabsRef.current.findIndex((tab) => tab === e.target);
+      index > -1 && tabsRef.current[0].focus();
+    }
+    if (e.key === "End") {
+      e.preventDefault(); // To prevent page from scrolling down
+      const index = tabsRef.current.findIndex((tab) => tab === e.target);
+      index > -1 && tabsRef.current[tabsLength - 1].focus();
+    }
+  };
   return (
     <div role="tablist" className={cn("tabs", className)}>
-      {children}
+      {React.Children.map(children, (tab: any, index: number) => {
+        return React.cloneElement(tab, {
+          ref: (el: HTMLElement) => (tabsRef.current[index] = el),
+          onKeyDown,
+        });
+      })}
     </div>
   );
 };
